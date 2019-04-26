5259a5a33885eeac338f8bee7ef3c136c899b1b4
Larry Ruckman <ruckman@slac.stanford.edu>
2019-04-26 13:24:19 -0700

    adding AtlasRd53FmcXilinxZcu102_WithoutPS_SFP_1GbE

---
+++ firmware/targets/AtlasRd53FmcXilinxZcu102_WithoutPS_SFP_1GbE/hdl/AtlasRd53FmcXilinxZcu102_WithoutPS_SFP_1GbE.vhd
@@ -0,0 +1,230 @@
+-------------------------------------------------------------------------------
+-- File       : AtlasRd53FmcXilinxZcu102_WithoutPS_SFP_1GbE.vhd
+-- Company    : SLAC National Accelerator Laboratory
+-------------------------------------------------------------------------------
+-- Description: Top Level Firmware Target
+-------------------------------------------------------------------------------
+-- This file is part of 'ATLAS RD53 FMC DEV'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'ATLAS RD53 FMC DEV', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+use work.StdRtlPkg.all;
+use work.AxiLitePkg.all;
+use work.AxiStreamPkg.all;
+use work.SsiPkg.all;
+use work.EthMacPkg.all;
+
+library unisim;
+use unisim.vcomponents.all;
+
+entity AtlasRd53FmcXilinxZcu102_WithoutPS_SFP_1GbE is
+   generic (
+      TPD_G        : time    := 1 ns;
+      SIMULATION_G : boolean := false;  
+      BUILD_INFO_G : BuildInfoType);
+   port (
+      -- FMC Interface
+      fmcHpc0LaP : inout slv(33 downto 0);
+      fmcHpc0LaN : inout slv(33 downto 0);
+      fmcHpc1LaP : inout slv(29 downto 0);
+      fmcHpc1LaN : inout slv(29 downto 0);
+      -- SFP Interface
+      sfpClk156P : in    sl;
+      sfpClk156N : in    sl;
+      sfpTxP     : out   slv(3 downto 0);
+      sfpTxN     : out   slv(3 downto 0);
+      sfpRxP     : in    slv(3 downto 0);
+      sfpRxN     : in    slv(3 downto 0));
+end AtlasRd53FmcXilinxZcu102_WithoutPS_SFP_1GbE;
+
+architecture TOP_LEVEL of AtlasRd53FmcXilinxZcu102_WithoutPS_SFP_1GbE is
+
+   constant DMA_CLK_FREQ_C : real := 125.0E+6;  -- Units of Hz
+   constant DMA_AXIS_CONFIG_C : AxiStreamConfigType := (
+      TSTRB_EN_C    => false,
+      TDATA_BYTES_C => 8,                       -- 64-bit data interface
+      TDEST_BITS_C  => 8,
+      TID_BITS_C    => 0,
+      TKEEP_MODE_C  => TKEEP_COMP_C,
+      TUSER_BITS_C  => 2,
+      TUSER_MODE_C  => TUSER_FIRST_LAST_C);
+
+   signal efuse    : slv(31 downto 0);
+   signal localMac : slv(47 downto 0);
+
+   signal ibMacMaster : AxiStreamMasterType;
+   signal ibMacSlave  : AxiStreamSlaveType;
+   signal obMacMaster : AxiStreamMasterType;
+   signal obMacSlave  : AxiStreamSlaveType;
+
+   signal dmaClk       : sl;
+   signal dmaRst       : sl;
+   signal dmaIbMasters : AxiStreamMasterArray(1 downto 0);
+   signal dmaIbSlaves  : AxiStreamSlaveArray(1 downto 0);
+   signal dmaObMasters : AxiStreamMasterArray(1 downto 0);
+   signal dmaObSlaves  : AxiStreamSlaveArray(1 downto 0);
+
+   signal sfpClk156     : sl;
+   signal iDelayCtrlRdy : sl;
+   signal refClk300MHz  : sl;
+   signal refRst300MHz  : sl;
+   signal phyReady  : sl;   
+
+   attribute IODELAY_GROUP                 : string;
+   attribute IODELAY_GROUP of U_IDELAYCTRL : label is "rd53_aurora";
+
+   attribute KEEP_HIERARCHY                 : string;
+   attribute KEEP_HIERARCHY of U_IDELAYCTRL : label is "TRUE";
+
+begin
+
+   --------------------------
+   -- Reference 300 MHz clock 
+   --------------------------
+   U_MMCM : entity work.ClockManagerUltraScale
+      generic map(
+         TPD_G              => TPD_G,
+         SIMULATION_G       => SIMULATION_G,
+         TYPE_G             => "MMCM",
+         INPUT_BUFG_G       => false,
+         FB_BUFG_G          => true,
+         RST_IN_POLARITY_G  => '1',
+         NUM_CLOCKS_G       => 2,
+         -- MMCM attributes
+         BANDWIDTH_G        => "OPTIMIZED",
+         CLKIN_PERIOD_G     => 6.4,
+         DIVCLK_DIVIDE_G    => 1,
+         CLKFBOUT_MULT_F_G  => 6.0,
+         CLKOUT0_DIVIDE_F_G => 3.125,   -- 300 MHz = 937.5 MHz/3.125
+         CLKOUT1_DIVIDE_G   => 6)       -- 156.25 MHz = 937.5 MHz/6   
+      port map(
+         clkIn     => sfpClk156,
+         clkOut(0) => refClk300MHz,
+         clkOut(1) => open,
+         rstOut(0) => refRst300MHz,
+         rstOut(1) => open);
+         
+   U_IDELAYCTRL : IDELAYCTRL
+      generic map (
+         SIM_DEVICE => "ULTRASCALE")
+      port map (
+         RDY    => iDelayCtrlRdy,
+         REFCLK => refClk300MHz,
+         RST    => refRst300MHz);
+         
+   U_1GigE : entity work.GigEthGthUltraScaleWrapper
+      generic map (
+         TPD_G              => TPD_G,
+         -- DMA/MAC Configurations
+         NUM_LANE_G         => 1,
+         -- QUAD PLL Configurations
+         USE_GTREFCLK_G     => false,
+         CLKIN_PERIOD_G     => 6.4,   -- 156.25 MHz
+         DIVCLK_DIVIDE_G    => 5,     -- 31.25 MHz = (156.25 MHz/5)
+         CLKFBOUT_MULT_F_G  => 32.0,  -- 1 GHz = (32 x 31.25 MHz)
+         CLKOUT0_DIVIDE_F_G => 8.0,   -- 125 MHz = (1.0 GHz/8)
+         -- AXI Streaming Configurations
+         AXIS_CONFIG_G      => (others=>EMAC_AXIS_CONFIG_C))
+      port map (
+         -- Local Configurations
+         localMac(0)     => localMac,
+         -- Streaming DMA Interface 
+         dmaClk(0)       => dmaClk,
+         dmaRst(0)       => dmaRst,
+         dmaIbMasters(0) => obMacMaster,
+         dmaIbSlaves(0)  => obMacSlave,
+         dmaObMasters(0) => ibMacMaster,
+         dmaObSlaves(0)  => ibMacSlave,
+         -- Misc. Signals
+         extRst          => '0',
+         phyClk          => dmaClk,
+         phyRst          => dmaRst,
+         phyReady(0)     => phyReady,
+         -- MGT Clock Port
+         gtClkP          => sfpClk156P,
+         gtClkN          => sfpClk156N,
+         -- MGT Ports
+         gtTxP(0)        => sfpTxP(0),
+         gtTxN(0)        => sfpTxN(0),
+         gtRxP(0)        => sfpRxP(0),
+         gtRxN(0)        => sfpRxN(0));       
+         
+   U_TERM_GTs : entity work.Gthe4ChannelDummy
+      generic map (
+         TPD_G   => TPD_G,
+         WIDTH_G => 3)
+      port map (
+         refClk => dmaClk,
+         gtRxP  => sfpRxP(3 downto 1),
+         gtRxN  => sfpRxN(3 downto 1),
+         gtTxP  => sfpTxP(3 downto 1),
+         gtTxN  => sfpTxN(3 downto 1));           
+         
+   U_EFuse : EFUSE_USR
+      port map (
+         EFUSEUSR => efuse);
+
+   localMac(23 downto 0)  <= x"56_00_08";  -- 08:00:56:XX:XX:XX (big endian SLV)   
+   localMac(47 downto 24) <= efuse(31 downto 8);
+
+   ----------------------
+   -- RUDP Wrapper Module
+   ----------------------
+   U_RUDP : entity work.RudpServers
+      generic map (
+         TPD_G             => TPD_G,
+         DMA_AXIS_CONFIG_G => DMA_AXIS_CONFIG_C,
+         CLK_FREQUENCY_G   => DMA_CLK_FREQ_C,
+         IP_ADDR_G         => x"0A02A8C0",  -- Set the default IP address before DHCP: 192.168.2.10 = x"0A02A8C0"
+         DHCP_G            => false,
+         JUMBO_G           => false)
+      port map (
+         -- Clock and Reset
+         dmaClk       => dmaClk,
+         dmaRst       => dmaRst,
+         -- ETH interface
+         localMac     => localMac,
+         ibMacMaster  => ibMacMaster,
+         ibMacSlave   => ibMacSlave,
+         obMacMaster  => obMacMaster,
+         obMacSlave   => obMacSlave,
+         -- DMA Interface
+         dmaObMasters => dmaObMasters,
+         dmaObSlaves  => dmaObSlaves,
+         dmaIbMasters => dmaIbMasters,
+         dmaIbSlaves  => dmaIbSlaves);
+         
+   -------------
+   -- FMC Module
+   -------------
+   U_App : entity work.AtlasRd53FmcCore
+      generic map (
+         TPD_G             => TPD_G,
+         BUILD_INFO_G      => BUILD_INFO_G,
+         SIMULATION_G      => SIMULATION_G,
+         DMA_AXIS_CONFIG_G => DMA_AXIS_CONFIG_C,
+         DMA_CLK_FREQ_G    => DMA_CLK_FREQ_C,
+         XIL_DEVICE_G      => "ULTRASCALE_PLUS")
+      port map (
+         -- I/O Delay Interfaces
+         iDelayCtrlRdy => iDelayCtrlRdy,
+         -- DMA Interface (dmaClk domain)
+         dmaClk        => dmaClk,
+         dmaRst        => dmaRst,
+         dmaObMasters  => dmaObMasters,  -- DMA[0] = DATA, DMA[1] = AXI-Lite Config
+         dmaObSlaves   => dmaObSlaves,
+         dmaIbMasters  => dmaIbMasters,
+         dmaIbSlaves   => dmaIbSlaves,
+         -- FMC LPC Ports
+         fmcLaP        => fmcHpc0LaP,
+         fmcLaN        => fmcHpc0LaN);
+
+end TOP_LEVEL;
