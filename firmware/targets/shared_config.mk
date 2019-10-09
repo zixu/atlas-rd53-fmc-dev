ae64c7e10232bc93053b8610024c46ed4244171a
Larry Ruckman <ruckman@slac.stanford.edu>
2019-10-09 15:22:04 -0700

    adding AtlasRd53Core.LocalReset register

--- firmware/targets/shared_config.mk
+++ firmware/targets/shared_config.mk
@@ -1,5 +1,5 @@
-# Define Firmware Version: v2.1.2.2
-export PRJ_VERSION = 0x02010202
+# Define Firmware Version: v2.1.2.3
+export PRJ_VERSION = 0x02010203
 
 # ZCU102 builds use .BIN files
 export GEN_BIN_IMAGE = 1
