5e60b96caceecfe7a398f2cc29416b1a85db0e7a
Larry Ruckman <ruckman@slac.stanford.edu>
2019-10-09 21:46:03 -0700

    updating Ultrscale RX IOSTANDARD from LVDS to DIFF_HSTL_I_18

--- firmware/targets/shared_config.mk
+++ firmware/targets/shared_config.mk
@@ -1,5 +1,5 @@
-# Define Firmware Version: v2.1.2.3
-export PRJ_VERSION = 0x02010203
+# Define Firmware Version: v2.1.2.4
+export PRJ_VERSION = 0x02010204
 
 # ZCU102 builds use .BIN files
 export GEN_BIN_IMAGE = 1
