a050cfdcbcb2dab14480de0f4db9bc7a376c08cf
Larry Ruckman <ruckman@slac.stanford.edu>
2019-10-11 11:14:18 -0700

    reverting AuroraRxGearboxAligner to 12a0d11

--- firmware/targets/shared_config.mk
+++ firmware/targets/shared_config.mk
@@ -1,5 +1,5 @@
-# Define Firmware Version: v2.1.2.5
-export PRJ_VERSION = 0x02010205
+# Define Firmware Version: v2.1.2.6
+export PRJ_VERSION = 0x02010206
 
 # ZCU102 builds use .BIN files
 export GEN_BIN_IMAGE = 1
