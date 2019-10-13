2b0100080f77cb21edc662e931fe79cd870a8d6a
Larry Ruckman <ruckman@slac.stanford.edu>
2019-10-12 18:04:16 -0700

    adding ability for ability to switch between automatic or manual delay control

--- firmware/targets/shared_config.mk
+++ firmware/targets/shared_config.mk
@@ -1,5 +1,5 @@
-# Define Firmware Version: v2.1.2.7
-export PRJ_VERSION = 0x02010207
+# Define Firmware Version: v2.1.2.8
+export PRJ_VERSION = 0x02010208
 
 # ZCU102 builds use .BIN files
 export GEN_BIN_IMAGE = 1
