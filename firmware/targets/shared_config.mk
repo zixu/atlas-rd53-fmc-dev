a3c569bf91646e555bd6191c0b0bd42bbfb97687
Larry Ruckman <ruckman@slac.stanford.edu>
2019-10-14 18:17:53 -0700

    adding slideDlyCfg and slideDlyDir

--- firmware/targets/shared_config.mk
+++ firmware/targets/shared_config.mk
@@ -1,5 +1,5 @@
-# Define Firmware Version: v2.1.2.8
-export PRJ_VERSION = 0x02010208
+# Define Firmware Version: v2.1.2.9
+export PRJ_VERSION = 0x02010209
 
 # ZCU102 builds use .BIN files
 export GEN_BIN_IMAGE = 1
