a6e919f7ec03c34f52e0368e3092373736f2fe1e
Larry Ruckman <ruckman@slac.stanford.edu>
2019-10-15 09:41:53 -0700

    adding EYE_SCAN_S to AuroraRxGearboxAligner.vhd

--- firmware/targets/shared_config.mk
+++ firmware/targets/shared_config.mk
@@ -1,5 +1,5 @@
-# Define Firmware Version: v2.1.2.9
-export PRJ_VERSION = 0x02010209
+# Define Firmware Version: v2.1.2.10
+export PRJ_VERSION = 0x02010210
 
 # ZCU102 builds use .BIN files
 export GEN_BIN_IMAGE = 1
