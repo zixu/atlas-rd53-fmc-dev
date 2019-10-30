dd76b10f3844106d58c8bacfe0dfab9fa37db8f5
Larry Ruckman <ruckman@slac.stanford.edu>
2019-10-30 16:35:08 -0700

    moving AtlasRd53HsSelectio.vhd to submodules/atlas-rd53-fw-lib

--- firmware/common/fmc/ruckus.tcl
+++ firmware/common/fmc/ruckus.tcl
@@ -8,18 +8,4 @@ set family [getFpgaFamily]
 loadSource           -dir  "$::DIR_PATH/rtl"
 loadSource -sim_only -dir  "$::DIR_PATH/tb"
 
-if { ${family} eq {artix7} ||
-     ${family} eq {kintex7} ||
-     ${family} eq {virtex7} ||
-     ${family} eq {zynq} } {
-   loadSource -dir "$::DIR_PATH/rtl/7Series"
-}
-
-if { ${family} eq {kintexu} ||
-     ${family} eq {kintexuplus} ||
-     ${family} eq {virtexuplus} ||
-     ${family} eq {zynquplus} } {
-   loadSource -dir "$::DIR_PATH/rtl/UltraScale"
-}
-
 set_property strategy Performance_ExplorePostRoutePhysOpt [get_runs impl_1]
