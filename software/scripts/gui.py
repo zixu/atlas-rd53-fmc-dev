861b30970813f83709a8cfcb4c827edcf5428a0c
Larry Ruckman <ruckman@slac.stanford.edu>
2020-04-13 11:03:43 -0700

    removed whitespace

--- software/scripts/gui.py
+++ software/scripts/gui.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 #-----------------------------------------------------------------------------
-# This file is part of the 'Camera link gateway'. It is subject to 
-# the license terms in the LICENSE.txt file found in the top-level directory 
-# of this distribution and at: 
-#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
-# No part of the 'Camera link gateway', including this file, may be 
-# copied, modified, propagated, or distributed except according to the terms 
+# This file is part of the 'Camera link gateway'. It is subject to
+# the license terms in the LICENSE.txt file found in the top-level directory
+# of this distribution and at:
+#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+# No part of the 'Camera link gateway', including this file, may be
+# copied, modified, propagated, or distributed except according to the terms
 # contained in the LICENSE.txt file.
 #-----------------------------------------------------------------------------
 import setupLibPaths
@@ -24,44 +24,44 @@ argBool = lambda s: s.lower() in ['true', 't', 'yes', '1']
 
 # Add arguments
 parser.add_argument(
-    "--dev", 
+    "--dev",
     type     = str,
     required = False,
     default  = '/dev/datadev_0',
     help     = "path to device",
-) 
+)
 
 parser.add_argument(
-    "--hwType", 
+    "--hwType",
     type     = str,
     required = False,
     default  = 'eth',
     help     = "Define whether sim/rce/pcie/eth HW config",
-)  
+)
 
 parser.add_argument(
-    "--ip", 
+    "--ip",
     type     = str,
     required = False,
     default  = '192.168.2.10',
     help     = "IP address for hwType=eth",
-) 
+)
 
 parser.add_argument(
-    "--pollEn", 
+    "--pollEn",
     type     = argBool,
     required = False,
     default  = True,
     help     = "Enable auto-polling",
-) 
+)
 
 parser.add_argument(
-    "--initRead", 
+    "--initRead",
     type     = argBool,
     required = False,
     default  = True,
     help     = "Enable read all variables at start",
-)  
+)
 
 # Get the arguments
 args = parser.parse_args()
